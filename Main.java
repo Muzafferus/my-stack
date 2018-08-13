5f0aad6c167fb989479ee512e014d0a03c3ac10a
Muzafferus <muzefferpasazade@gmail.com>
2018-08-13 11:34:23 +0400

    Using Stack with array

--- Main.java
+++ Main.java
@@ -1,22 +1,27 @@
-﻿public class Main {
+﻿
+public class Main {
 	public static void main(String[] args) {
-		 
-		 MyStack<Integer> stack = new MyStack();
-		 		 
-		 stack.push(2005);
-		 stack.push(2007);
-		 stack.push(2009);
-		 
-		 System.out.println("My stack list: " + stack);
-		 
-		 System.out.println("Last stack element: " + stack.peek());
-		 System.out.println("My stack list: " + stack);
-			 
-		 System.out.println("Last stack element (with delete): " + stack.pop());
-		 System.out.println("My stack list: " + stack);
-		 
-		 
+
+		MyStack stack = new MyStack();
+
+		System.out.println("My stack list empty: " + stack.isEmpty());
+		System.out.println("My stack list: " + stack);
+		stack.push(2005);
+		stack.push(2007);
+		stack.push(2009);
+
+		System.out.println("My stack list: " + stack);
+		System.out.println("My stack list empty: " + stack.isEmpty());
+
+		System.out.println("Last stack element: " + stack.peek());
+		System.out.println("My stack list: " + stack);
+
+		System.out.println("Last stack element (with delete): " + stack.pop());
+		System.out.println("My stack list: " + stack);
+
 		System.out.println("Search 2010: " + stack.search(2010));
-		System.out.println("Search 2005: " + stack.search(2005));	
+		System.out.println("Search 2005: " + stack.search(2005));
+		System.out.println("My stack list: " + stack);
+
 	}
 }
--- MyStack.java
+++ MyStack.java
@@ -1,40 +1,83 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class MyStack<E> {
-	List<E> list = new ArrayList<>();
-	
+public class MyStack {
+	static int i = 0;
+	static Integer[] list = new Integer[0];
+
 	public MyStack() {
 	}
 
 	public boolean isEmpty() {
-		return list.isEmpty();
-	}
-
-	public void push(E item) {
-		list.add(item);
-	}
-
-	public E peek() {
-		return list.get(list.size() - 1);
-	}
-
-	public E pop() {
-		E item = list.get(list.size() - 1);
-		list.remove(list.size() - 1);
-		return item;
-	}
-
-	public int search(E item) {
-		
-		int index = list.indexOf(item);
-		if(index == -1) {
-			return index;
+		boolean empty = true;
+		for (Integer item : list) {
+			if (item != null) {
+				empty = false;
+				break;
+			}
 		}
-		return (index + 1);
+		return empty;
 	}
-	
+
+	public void push(Integer item) {
+
+		Integer[] oldList = new Integer[i];
+		for (int j = 0; j < list.length; j++) {
+			oldList[j] = list[j];
+		}
+
+		i++;
+		list = new Integer[i];
+
+		for (int j = 0; j < oldList.length; j++) {
+			list[j] = oldList[j];
+		}
+
+		list[i - 1] = item;
+	}
+
+	public Integer peek() {
+		return list[list.length - 1];
+	}
+
+	public Integer pop() {
+
+		Integer lastItem = list[list.length - 1];
+
+		Integer[] oldList = new Integer[i];
+		for (int j = 0; j < list.length; j++) {
+			oldList[j] = list[j];
+		}
+
+		i--;
+		list = new Integer[i];
+
+		for (int j = 0; j < oldList.length - 1; j++) {
+			list[j] = oldList[j];
+		}
+
+		return lastItem;
+	}
+
+	public int search(Integer item) {
+		boolean search = false;
+		int index = 0;
+		for (int j = 0; j < list.length; j++) {
+			if (item.equals(list[j])) {
+				index = j;
+				search = true;
+			}
+		}
+		if (!search) {
+			return -1;
+		} else {
+			return (index + 1);
+		}
+	}
+
 	public String toString() {
-		return list.toString();
+		String res = Arrays.toString(list);
+		return res;
 	}
 }
